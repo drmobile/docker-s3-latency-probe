b3c9e2d2bc2eb7801ff0cdaece164b890550cb6e
Jonas Cheng <jonas.cheng@gmail.com>
2017-05-15 10:14:54 +0800

    carry default region from env

--- s3-latency-probe-cron.sh
+++ s3-latency-probe-cron.sh
@@ -9,5 +9,5 @@ mkdir -p ${LOG}
 
 docker rm ${NAME} -f
 docker pull ${IMAGE}
-docker run --name ${NAME} -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e S3_BUCKET_NAME=${BUCKET} -v ${LOG}:/var/log ${IMAGE}
+docker run --name ${NAME} -e AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -e S3_BUCKET_NAME=${BUCKET} -v ${LOG}:/var/log ${IMAGE}
 
